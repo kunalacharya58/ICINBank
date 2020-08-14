772ea5e1c37d94abe0eaf33136f06188ee9281af
Aditya Menon <adityamenon3.nmims@gmail.com>
2020-08-14 15:06:15 +0530

    added route guarding

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'user', component: BaseComponent, children:[
+    { path: "", redirectTo: 'home', pathMatch:'full'},
     {path: 'home', component: HomeComponent},
     {path: 'deposit', component: DepositComponent }
   ]},
--- ui/src/app/auth/components/login/login.component.ts
+++ ui/src/app/auth/components/login/login.component.ts
@@ -1,76 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
-import { AuthService } from "../../auth.service";
+import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  
-  usernameError = false
-  usernameErrorMessage = "Username should contain only alphabets or numbers"
+  usernameError = false;
+  usernameErrorMessage = 'Username should contain only alphabets or numbers';
 
-  passError = false
-  passErrorMessage = "must contain atleast 6 characters"
-  
-  
-  form:FormGroup
-  error = false
-  errorMsg = ""
-  constructor(public fb:FormBuilder, private auth:AuthService, private router:Router) {
+  passError = false;
+  passErrorMessage = 'must contain atleast 6 characters';
+
+  form: FormGroup;
+  error = false;
+  errorMsg = '';
+
+  loggedIn=false
+  constructor(
+    public fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
     this.form = this.fb.group({
       username: [''],
       password: [''],
-    })
+    });
+    if(localStorage.getItem('userId') == '' || localStorage.getItem('userId') == null) {
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = true;
+    }
   }
 
-
-
-
-  submit(){
+  submit() {
     this.auth.login(this.form.value).subscribe(
-      (resp) => { if(resp.ok) this.router.navigate(['user/home']) },
-      (err:HttpErrorResponse) => {
+      (resp) => {
+        if (resp.ok) {
+          this.loggedIn = true;
+          localStorage.setItem('userId', resp.headers.get('userID'));
+          this.router.navigate(['user']);
+        }
+      },
+      (err: HttpErrorResponse) => {
         this.errorMsg = err.headers.get('message');
-        this.error = true
-     }
-    )
+        this.error = true;
+      }
+    );
   }
   ngOnInit(): void {
+    if(localStorage.getItem('userId') == '' || localStorage.getItem('userId') == null) {
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = true;
+      this.router.navigate(['user']);
+    }
   }
 
-  haveSpecialCharacters(str : string){
+  haveSpecialCharacters(str: string) {
     let regex = new RegExp(/^[0-9a-zA-Z]+$/);
     return !regex.test(str);
   }
-  
-  isEmpty(str : string){
-    return (!str || str.length === 0 || str.length === undefined)
+
+  isEmpty(str: string) {
+    return !str || str.length === 0 || str.length === undefined;
   }
 
-  validateUserName(){ 
-    
-    let username = this.form.get('username').value
-    this.usernameError = this.haveSpecialCharacters(username) && !this.isEmpty(username)
-    return !this.usernameError
+  validateUserName() {
+    let username = this.form.get('username').value;
+    this.usernameError =
+      this.haveSpecialCharacters(username) && !this.isEmpty(username);
+    return !this.usernameError;
   }
 
-  validatePassword(){
-
-    let password = this.form.get('password').value
-    this.passError = (password.length < 6) && !this.isEmpty(password)
-    return !this.passError
+  validatePassword() {
+    let password = this.form.get('password').value;
+    this.passError = password.length < 6 && !this.isEmpty(password);
+    return !this.passError;
   }
 
-  validateFormAndSubmit(){
-    if(!this.validateUserName())
-        document.getElementById("lCustId").focus();
-    else if(!this.validatePassword())
-        document.getElementById("lpass").focus();
-    else{
+  validateFormAndSubmit() {
+    if (!this.validateUserName()) document.getElementById('lCustId').focus();
+    else if (!this.validatePassword()) document.getElementById('lpass').focus();
+    else {
       this.submit();
     }
   }
