ac4a639f0332a86c7cf34cf4912fb93dffd486b4
Aditya Menon <adityamenon3.nmims@gmail.com>
2020-08-15 13:42:08 +0530

    working backend

--- src/main/java/com/project/IcinBankBackendApplication.java
+++ src/main/java/com/project/IcinBankBackendApplication.java
@@ -10,32 +10,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.project.model.User;
 import com.project.service.AccountService;
 import com.project.service.RegisterService;
-
+//implements CommandLineRunner
 @SpringBootApplication
-public class IcinBankBackendApplication implements CommandLineRunner {
+public class IcinBankBackendApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IcinBankBackendApplication.class, args);
 	}
-	
-	@Autowired
-	RegisterService regServ;
-	
-	@Autowired
-	AccountService accServ;
+}	
+//	@Autowired
+//	RegisterService regServ;
+//	
+//	@Autowired
+//	AccountService accServ;
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		LocalDate date = LocalDate.now();
+//		User user = new User("John", "Doe", "0987654321", "johndoe@gmail.com", "johndoe", "secretpass", date, "street");
+//		
+//		regServ.createUser(user);
+//		
+//		accServ.deposit(2500, "primary", user.getId());
+//		accServ.deposit(3000, "savings", user.getId());
+//		
+//		accServ.withdraw(300, "primary", user.getId());
+//		accServ.withdraw(500, "savings", user.getId());
+//	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		LocalDate date = LocalDate.now();
-		User user = new User("John", "Doe", "0987654321", "johndoe@gmail.com", "johndoe", "secretpass", date, "street");
-		
-		regServ.createUser(user);
-		
-		accServ.deposit(2500, "primary", user.getId());
-		accServ.deposit(3000, "savings", user.getId());
-		
-		accServ.withdraw(300, "primary", user.getId());
-		accServ.withdraw(500, "savings", user.getId());
-	}
-
-}
+//}
