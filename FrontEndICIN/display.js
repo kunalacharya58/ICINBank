578bf49c97c89eeff9bef0432859c1127b0470b2
Dhwani Patel <dhwani29@somaiya.edu>
2020-08-16 13:20:53 +0530

    updation of same bank option[F

--- FrontEndICIN/display.js
+++ FrontEndICIN/display.js
@@ -2,14 +2,26 @@
 $(function() {
     $('input[name="transactionType"]').on('click', function() {
         if ($(this).val() == 'self') {
-            $('#form-2').hide();
+
             $('#form-1').show();
+            $('#form-2').hide();
+            $('#form-3').hide();
+            
 
         }
-        else {
+        if  ($(this).val() == 'same') {
             $('#form-1').hide();
             $('#form-2').show();
+            $('#form-3').hide();
         }
+
+        if($(this).val() == 'other'){
+            $('#form-1').hide();
+            $('#form-2').hide();
+            $('#form-3').show();
+        }
+
+
     });
 });
 
